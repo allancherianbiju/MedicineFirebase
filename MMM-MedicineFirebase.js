0388fd219cc76a78fde15ff0fe074f2a8964e2da
allancherianbiju <allancherianbiju@gmail.com>
2020-03-20 20:04:12 +0530

    first commit

--- MMM-MedicineFirebase.js
+++ MMM-MedicineFirebase.js
@@ -1,5 +1,6 @@
 Module.register("MMM-MedicineFirebase",{
   defaults: {
+    message: '',
     firebaseDatabaseRootRef: '/medicines/106336659285619048398',
     title: 'Medicines',
   },
@@ -22,54 +23,7 @@ Module.register("MMM-MedicineFirebase",{
     Log.info(`[${this.name}]: SEND_CONFIG`, this.config);
     //this.sendSocketNotification('SEND_CONFIG', this.config);
   },
-  getDom: function() {
-    var medicine;
-    const wrapper = document.createElement('div');
-    wrapper.innerHTML = `
-      <h2 class="title">Medicines</h2>`;
-    var subElement = document.createElement("div");
-      subElement.id = "DETAILS";
-    //return subElement;
-
-      if (medicine === null) {
-      subElement.innerHTML =
-        '<div class="loading"><span class="zmdi zmdi-rotate-right zmdi-hc-spin"></span> Loading...</div>';
-      return subElement;
-    }
-    return wrapper;
-
-    //I'm not sure whether I want to pass the payload over here and then display the values or display the values down in the 
-    //NotificationReceived method. I'm not doing it right, so both aren't working anyway. :(
-    
-    subElement.innerHTML = `
-      <h2 class="title">Medicines</h2>
-      <ul class="attributes">
-        <li class="attribute">
-          <!--<span class="icon zmdi zmdi-user zmdi-hc-fw"></span>-->
-          <span class="name">Medicine Name</span>
-          <span class="value">${medicine.medName}</span>
-        </li>
-        <li class="attribute">
-          <!--<span class="icon zmdi zmdi-car zmdi-hc-fw"></span>-->
-          <span class="name">Day</span>
-          <span class="value">${medicine.medDay}</span>
-        </li>
-        <li class="attribute">
-          <!--<span class="icon zmdi zmdi-clock-outline-alt zmdi-hc-fw"></span>-->
-          <span class="name">Time</span>
-          <span class="value">${medicine.medTime}</span>
-        </li>
-        <li class="attribute">
-          <!--<span class="icon zmdi zmdi-food-outline zmdi-hc-fw"></span>-->
-          <span class="name">Food</span>
-          <span class="value">${medicine.medFood}</span>
-        </li>
-		  </ul>
-		`;
-    wrapper.appendChild(subElement);
-    return subElement;
-    
-  },
+  
   
   socketNotificationReceived: function(notification, payload) {
     Log.info(
@@ -78,14 +32,40 @@ Module.register("MMM-MedicineFirebase",{
     );
 
     if (notification === 'MEDICINE_ADDED'){
-      var elem = document.getElementById("DETAILS");
-      elem.innerHTML = `
+      this.config.message = payload;
+      this.updateDom();
+      }
+    if (notification === 'MEDICINES_CHANGED') {
+      this.medicine = payload;
+      return this.updateDom();
+    }
+
+    return false;
+  },
+  
+  getDom: function() {
+    const wrapper = document.createElement('div');
+    wrapper.id = "MAIN";
+    wrapper.innerHTML = `
+      <h2 class="title">Medicines</h2>`;
+
+      if (this.config === null) {
+      wrapper.innerHTML =
+        '<div class="loading"><span class="zmdi zmdi-rotate-right zmdi-hc-spin"></span> Loading...</div>';
+      return wrapper;
+    }
+    return wrapper;
+
+    //I'm not sure whether I want to pass the payload over here and then display the values or display the values down in the 
+    //NotificationReceived method. I'm not doing it right, so both aren't working anyway. :(
+    
+    wrapper.innerHTML = `
       <h2 class="title">Medicines</h2>
       <ul class="attributes">
         <li class="attribute">
           <!--<span class="icon zmdi zmdi-user zmdi-hc-fw"></span>-->
           <span class="name">Medicine Name</span>
-          <span class="value">${medicine.medName}</span>
+          <span class="value">${this.config.medName}</span>
         </li>
         <li class="attribute">
           <!--<span class="icon zmdi zmdi-car zmdi-hc-fw"></span>-->
@@ -104,15 +84,6 @@ Module.register("MMM-MedicineFirebase",{
         </li>
 		  </ul>
 		`;
-    return elem;
-      this.medicine = payload;
-      return this.updateDom();
-      }
-    if (notification === 'MEDICINES_CHANGED') {
-      this.medicine = payload;
-      return this.updateDom();
-    }
-
-    return false;
+    return wrapper;
   },
 });
